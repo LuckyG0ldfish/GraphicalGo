358a05ba1caac7e270d6c071697c9d6264c3fc28
LuckyG0ldfish <tim-koeneke@online.de>
2022-06-26 15:37:37 +0200

    build test

--- main.go
+++ main.go
@@ -1,26 +1,38 @@
 package main
 
 import (
-	"fmt"
+	// "github.com/AllenDang/giu"
 
-	"github.com/AllenDang/giu"
+	// "github.com/LuckyG0ldfish/GraphicalGo/ui"#
 
-	"github.com/LuckyG0ldfish/GraphicalGo/ui"
+	app "fyne.io/fyne/v2/app"
+	container "fyne.io/fyne/v2/container"
+	widget "fyne.io/fyne/v2/widget"
 )
 
 func main() {
 
-	wnd := giu.NewMasterWindow("Splitter", 800, 600, 0)
-	ui.Build(wnd)
+	// wnd := giu.NewMasterWindow("Splitter", 800, 600, 0)
+	// ui.Build(wnd)
 	test := "TestName"
 	createProject(test)
 
 }
 
 func createProject(name string) {
-	fmt.Println(name)
+	a := app.New()
+	w := a.NewWindow(name)
 
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
 
+	w.ShowAndRun()
+	
 }
 
 
