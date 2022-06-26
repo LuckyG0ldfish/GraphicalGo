c22e4c56f5ee158c21e93493167cbd581e5e0452
LuckyG0ldfish <tim-koeneke@online.de>
2022-06-26 17:28:13 +0200

    grphics start

--- main.go
+++ main.go
@@ -5,29 +5,38 @@ import (
 
 	// "github.com/LuckyG0ldfish/GraphicalGo/ui"#
 
-	app "fyne.io/fyne/v2/app"
-	container "fyne.io/fyne/v2/container"
-	widget "fyne.io/fyne/v2/widget"
+	// "image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	// "fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 
 	// wnd := giu.NewMasterWindow("Splitter", 800, 600, 0)
 	// ui.Build(wnd)
-	test := "TestName"
-	createProject(test)
+	createProject("GraphicalGo")
 
 }
 
 func createProject(name string) {
 	a := app.New()
 	w := a.NewWindow(name)
-
+	
 	hello := widget.NewLabel("Hello Fyne!")
+	// can := canvas.NewCircle(color.GrayModel.Convert(color.Black))
+	// tt := new([]fyne.Widget)
+	// define size 
+	w.Resize(fyne.NewSize(500, 400))
 	w.SetContent(container.NewVBox(
 		hello,
+		
 		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
+			hello.Hidden = true
+			widget.NewEntry()// hello.Move(fyne.NewPos(30, 30 )) // SetText("Welcome :)")
 		}),
 	))
 
